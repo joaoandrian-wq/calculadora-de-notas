2062984754ffab37e20fd9f8998dcbcbcb7d8af8
joaoandrian-wq <joao.andrian@escola.pr.gov.br>
2026-09-25 13:35:05 +0000

    commit insano

---
+++ script.js
@@ -0,0 +1,13 @@
+function calcularMedia (){
+    const textoNota1 = document. getElementbyId ("nota1").value;
+    const textoNota1 = document. getElementbyId ("nota2").value;
+    const textoNota1 = document. getElementbyId ("nota3").value;
+
+const nota1 = Number (textoNota1);
+const nota2 = Number (textoNota2);
+const nota3 = Number (textoNota3);
+
+const media = (nota1 + nota2 + nota3) / 3;
+let situacao; 
+if (media
+}
